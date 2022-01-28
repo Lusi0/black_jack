b35cebe58fd8aa9548581937edd7604df30a9554
Lusi0 <ItsLusio@gmail.com>
2022-01-28 11:14:12 -0800

    nice

--- black_jack.py
+++ black_jack.py
@@ -1,4 +1,8 @@
+import imp
 import os
+from queue import PriorityQueue
+import sys
+import time
 
 class card:
     def __init__(self, suit, rank, value):
@@ -105,6 +109,7 @@ class hand_dealer(hand):
         myaces = self.aces
         if self.cards[0].value == "special":
             myaces -= 1
+            total -= 11
         while total > 21 and myaces > 0:
             total -= 10
             myaces -= 1
@@ -132,7 +137,7 @@ class player:
     
     def hit_or_stand(self, deck):
         while True:
-            x = input("Do you want to hit or stand? (h/s) ")
+            x = slowinput("Do you want to hit or stand? (h/s) ", "white")
             if x == "h":
                 y = self.hit(deck)
                 if y == False:
@@ -141,10 +146,10 @@ class player:
             elif x == "s":
                 return False
             else:
-                print("Please enter h or s")
+                slowprint("Please enter h or s", "red")
 
     def chips_value(self):
-        print("You have {} chips".format(self.chips))
+        slowprint("You have {} chips".format(self.chips),"blue")
 
     def __str__(self):
         return "Player hand: {}".format(self.hand.hand_cards())
@@ -183,12 +188,12 @@ class game:
         self.dealer.hit(self.deck)
     
     def show_cards_all_hidden(self):
-        print("Player hand: {} (value: {})".format(self.player.hand.hand_cards(), self.player.hand.real_value_calculator()))
-        print("Dealer showing: {} (value: {})".format(self.dealer.hand.hand_cards_hidden(), self.dealer.hand.hand_value_hidden()))
+        slowprint("Player hand: {} (value: {})".format(self.player.hand.hand_cards(), self.player.hand.real_value_calculator()), "yellow")
+        slowprint("Dealer showing: {} (value: {})".format(self.dealer.hand.hand_cards_hidden(), self.dealer.hand.hand_value_hidden()), "magenta")
 
     def show_cards_all_revealed(self):
-        print("Player hand: {} (value: {})".format(self.player.hand.hand_cards(), self.player.hand.real_value_calculator()))
-        print("Dealer hand: {} (value: {})".format(self.dealer.hand.hand_cards(), self.dealer.hand.real_value_calculator()))
+        slowprint("Player hand: {} (value: {})".format(self.player.hand.hand_cards(), self.player.hand.real_value_calculator()), "yellow")
+        slowprint("Dealer hand: {} (value: {})".format(self.dealer.hand.hand_cards(), self.dealer.hand.real_value_calculator()), "magenta")
 
     def player_turn(self):
         return self.player.hit_or_stand(self.deck)
@@ -198,7 +203,9 @@ class game:
 
     def deck_plus_hidden(self):
         temp_deck = deck()
-        temp_deck.cards = self.deck.cards
+        temp_deck.cards = []
+        for card in self.deck.cards:
+            temp_deck.cards.append(card)
         temp_deck.cards.append(self.dealer.hand.cards[0])
         return temp_deck
     
@@ -215,7 +222,7 @@ class game:
                 values[card.value] = 1
         probabilities = {}
         for key in values:
-            probabilities[key] = round(values[key] / len(mydeck.cards), 5)*100
+            probabilities[key] = round(values[key] / len(mydeck.cards), 4)*100
         return probabilities
 
     def p2(self):
@@ -230,7 +237,7 @@ class game:
                     total_hit += pro[key]
             elif int(key) + value_showing < hit_under:
                 total_hit += pro[key]
-        return round(total_hit, 3)
+        return round(total_hit, 2)
 
     def p3(self):
         # probability you will bust if you hit
@@ -243,7 +250,7 @@ class game:
                     total_bust += pro[key]
             elif int(key) + hand_value > 21:
                 total_bust += pro[key]
-        return round(total_bust, 3)
+        return round(total_bust, 2)
 
     def p4(self):
         # probability your hand is better than the dealers
@@ -257,7 +264,7 @@ class game:
                     total_better += pro[key]
             elif int(key) + dealer_value < hand_value:
                 total_better += pro[key]
-        return round(total_better, 3)
+        return round(total_better, 2)
 
     def p5(self):
         # what is the probability that the dealer will bust if they hit
@@ -286,27 +293,58 @@ class game:
                         total_bust += pro_2d[key_lib][key] * pro[key_lib]
                 elif int(key) + int(key_lib) + self.dealer.hand.hand_value_hidden() > 21:
                     total_bust += pro_2d[key_lib][key] * pro[key_lib]
-        return round(total_bust*100, 3)
+        return round(total_bust*100, 2)
+    
     
 
         
 
                 
-
+def slowprint(s, color):
+    # change the color
+    if color == "red":
+        print("\033[91m", end="")
+    elif color == "green":
+        print("\033[92m", end="")
+    elif color == "yellow":
+        print("\033[93m", end="")
+    elif color == "blue":
+        print("\033[94m", end="")
+    elif color == "magenta":
+        print("\033[95m", end="")
+    elif color == "cyan":
+        print("\033[96m", end="")
+    elif color == "white":
+        print("\033[97m", end="")
+    elif color == "black":
+        print("\033[90m", end="")
+    elif color == "reset":
+        print("\033[0m", end="")
+    # print the string
+    for c in s:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.01)
+    sys.stdout.write('\n')
         
+def slowinput(s, color):
+    slowprint(s, color)
+    return input()
 
 
+def reset_color():
+    print("\033[0m", end="")
 
     
 def take_bet(chips):
     while True:
         try:
-            chip = int(input("How many chips would you like to bet? "))
+            chip = int(slowinput("How many chips would you like to bet? ", "white"))
         except:
-            print("Please enter an integer")
+            slowprint("Please enter an integer", "white")
         else:
             if chip > chips:
-                print("You don't have enough chips")
+                slowprint("You don't have enough chips", "red")
             else:
                 return chip
 
@@ -317,56 +355,64 @@ def main(game_instance):
         bet = take_bet(game_instance.player.chips)
         game_instance.deal_cards()
         game_instance.show_cards_all_hidden()
-        print(game_instance.p1())
-        print("The dealer has a {}% chance of hitting".format(game_instance.p2()))
-        print("You have a {}% chance of busting if you hit".format(game_instance.p3()))
-        print("Your hand is better than the dealers {}% of the time".format(game_instance.p4()))
-        print("The dealer has a {}% chance of busting if they hit".format(game_instance.p5()))
-        h = game_instance.player_turn()
-        while h:
-            game_instance.show_cards_all_hidden()
-            if game_instance.player.hand.real_value_calculator() > 21:
-                print("You busted!")
-                break
-            print("You have a {}% chance of busting if you hit".format(game_instance.p3()))
-            print("Your hand is better than the dealers {}% of the time".format(game_instance.p4()))
-            print("The dealer has a {}% chance of busting if they hit".format(game_instance.p5()))
+        if game_instance.player.hand.real_value_calculator() == 21:
+            slowprint("BLACKJACK!", "green")
+        else:
+            slowprint("{}".format(game_instance.p1()), "blue")
+            slowprint("The dealer has a {}% chance of hitting".format(game_instance.p2()), "blue")
+            slowprint("You have a {}% chance of busting if you hit".format(game_instance.p3()), "blue")
+            slowprint("Your hand is better than the dealers {}% of the time".format(game_instance.p4()), "blue")
+            slowprint("The dealer has a {}% chance of busting if they hit".format(game_instance.p5()), "blue")
             h = game_instance.player_turn()
+            while h:
+                game_instance.show_cards_all_hidden()
+                if game_instance.player.hand.real_value_calculator() > 21:
+                    slowprint("You busted!", "red")
+                    break
+                elif game_instance.player.hand.real_value_calculator() == 21:
+                    slowprint("BLACKJACK!", "green")
+                    break
+                slowprint("You have a {}% chance of busting if you hit".format(game_instance.p3()), "blue")
+                slowprint("Your hand is better than the dealers {}% of the time".format(game_instance.p4()),   "blue")
+                slowprint("The dealer has a {}% chance of busting if they hit".format(game_instance.p5()), "blue")
+                h = game_instance.player_turn()
        
 
-            
+        
         if game_instance.player.hand.real_value_calculator() == 21:
             game_instance.show_cards_all_revealed()
             game_instance.player.chips += bet
-            print("You won {} chips".format(bet))
-        elif game_instance.player.hand.real_value_calculator() < 21:
+            slowprint("You won {} chips".format(bet), "green")
+        else:
             j = game_instance.dealer_turn()
             while j:
                 if game_instance.dealer.hand.real_value_calculator() > 21:
-                    print("Dealer busted!")
+                    slowprint("Dealer busted!", "green")
                     break
                 j = game_instance.dealer_turn()
             game_instance.show_cards_all_revealed()
-        elif game_instance.player.hand.real_value_calculator() > 21:
-            game_instance.player.chips -= bet
-            print("You lost {} chips".format(bet))
-        elif game_instance.dealer.hand.real_value_calculator() > 21:
-            game_instance.player.chips += bet
-            print("You won {} chips".format(bet))
-        elif game_instance.player.hand.real_value_calculator() > game_instance.dealer.hand.real_value_calculator():
-            game_instance.player.chips += bet
-            print("You won {} chips".format(bet))
-        else:
-            game_instance.player.chips -= bet
-            print("You lost {} chips".format(bet))
+            if game_instance.player.hand.real_value_calculator() == game_instance.dealer.hand.real_value_calculator() < 21:
+                slowprint("It's a tie!", "yellow")
+            elif game_instance.player.hand.real_value_calculator() > 21:
+                game_instance.player.chips -= bet
+                slowprint("You lost {} chips".format(bet), "red")
+            elif game_instance.dealer.hand.real_value_calculator() > 21:
+                game_instance.player.chips += bet
+                slowprint("You won {} chips".format(bet), "green")
+            elif game_instance.player.hand.real_value_calculator() > game_instance.dealer.hand.real_value_calculator():
+                game_instance.player.chips += bet
+                slowprint("You won {} chips".format(bet), "green")
+            else:
+                game_instance.player.chips -= bet
+                slowprint("You lost {} chips".format(bet), "red")
 
         if game_instance.player.chips <= 0:
-            print("You are out of chips")
-            with open("save.txt", "w") as f:
-                    f.write(str(game_instance.player.chips))
+            slowprint("You are out of chips", "red")
+            print("\033[0m", end="")
+            update_save(game_instance.player.chips)
             break
         else:
-            x = input("Do you want to play again? (y/n) ")
+            x = slowinput("Do you want to play again? (y/n) ", "white")
             if x == "y":
                 game_instance.player.hand.empty_hand()
                 game_instance.dealer.hand.empty_hand()
@@ -374,20 +420,48 @@ def main(game_instance):
                 game_instance.deck.shuffle()
                 continue
             else:
-                print("You have {} chips".format(game_instance.player.chips))
+                slowprint("You have {} chips".format(game_instance.player.chips), "blue")
                 # save the chips to a file named save.txt
-                with open("save.txt", "w") as f:
-                    f.write(str(game_instance.player.chips))
+                print("\033[0m", end="")
+                update_save(game_instance.player.chips)
                 break
 
         
 
-
+slowprint("Welcome to Blackjack!", "magenta")
+slowprint("The goal of the game is to get as close to 21 as possible without going over", "magenta")
+name = slowinput("What is your name?", "white")
 # check if there is a save file and if there is load the chips
-chips = 100
 if os.path.isfile("save.txt"):
     with open("save.txt", "r") as f:
-        chips = int(f.read())
+        saves = f.read()
+
+saveslib = {
+
+}
+if len(saves) > 0:
+    saves = saves.split("\n")
+    for line in saves:
+        if line != "":
+            line = line.split(",")
+            saveslib[line[0]] = int(line[1])
+
+if name in saveslib:
+    slowprint("Welcome back {}!".format(name), "magenta")
+    chips = saveslib[name]
+else:
+    slowprint("Hello {}!".format(name), "magenta")
+    saveslib[name] = 100
+    chips = saveslib[name]
+
+def write_save():
+    with open("save.txt", "w") as f:
+        for line in saveslib:
+            f.write(line + "," + str(saveslib[line]) + "\n")
+
+def update_save(chips):
+    saveslib[name] = chips
+    write_save()
 
 if chips <= 0:
     chips = 100
